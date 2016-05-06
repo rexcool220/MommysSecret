2cade8a862a9fcdb4130122b3c9f426d2c786761
MommysSecret <rexcool220@gmail.com>
2016-05-07 01:35:27 +0800

    check in 20160506

--- Client.php
+++ Client.php
@@ -1,14 +1,8 @@
 <?php
 $message = "Hello Server";
 echo "Message To server :".$message;
-// create socket
-$socket = socket_create(AF_UNIX, SOCK_STREAM, 0) or die("Could not create socket\n");
-// connect to server
-try {
-	$result = socket_connect($socket, 'http://localhost/MommysSecret/mysock') or die("Could not connect to server\n");	
-} catch (Exception $e) {
-	echo $e->getMessage();
-}
+$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+$connection = socket_connect($socket,'127.0.0.1', 1234); 
 
 // send string to server
 socket_write($socket, $message, strlen($message)) or die("Could not send data to server\n");
