0b61c213867637a1e1556ea15342b5f3e3ee9451
Rex Gu <rex.gu@smartmatic.com>
2016-09-09 14:41:09 +0800

    +Add FBParser.php, FBParserCallBack.php

---
+++ FBParser.php
@@ -0,0 +1,31 @@
+<?php
+	require_once __DIR__ . '/vendor/autoload.php';
+	if(!session_id()) {
+		session_start();
+	}
+	if($_GET['googleFormUrl'] == '')
+	{
+		echo 'Google form url is empty';
+		//exit;
+	}
+	
+	$fb = new Facebook\Facebook([
+	  'app_id' => '1540605312908660',
+	  'app_secret' => '066f0c1bd42b77412f8d36776ee7b788',
+	  'default_graph_version' => 'v2.6',
+	]);
+	$helper = $fb->getRedirectLoginHelper();
+	
+	$permissions = ['email','user_managed_groups']; // optional
+	//$permissions = ['email','publish_actions','user_managed_groups']; // optional
+	
+	$loginUrl = $helper->getLoginUrl('http://mommyssecret.tw/FBParserCallBack.php', $permissions);
+	 
+	$_SESSION['googleFormUrl'] = $_GET['googleFormUrl'];
+	$_SESSION['fieldID'] = $_GET['fieldID'];
+	$_SESSION['facebookID'] = $_GET['facebookID'];
+	$_SESSION['groupID'] = $_GET['groupID'];
+	
+	//echo urldecode($loginUrl);
+	header("location: ".$loginUrl);
+?>
---
+++ FBParserCallBack.php
@@ -0,0 +1,138 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+include_once "./vendor/google/apiclient/examples/templates/base.php";
+require_once 'ConnectMySQL.php';
+header("Content-Type:text/html; charset=utf-8");
+if(!session_id()) {
+    session_start();
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>FBParser</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="//code.jquery.com/ui/1.12.0/themes/base/jquery-ui.css">
+	<link rel="stylesheet" type="text/css" href="MommysSecret.css?20160825">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<script src="https://code.jquery.com/ui/1.12.0/jquery-ui.js"></script>  
+</head>
+<body>
+<?php 
+if(!$accessToken)
+{
+	$fb = new Facebook\Facebook([
+			'app_id' => '1540605312908660',
+			'app_secret' => '066f0c1bd42b77412f8d36776ee7b788',
+			'default_graph_version' => 'v2.6',
+	]);
+	$helper = $fb->getRedirectLoginHelper();
+	try {
+		$accessToken = $helper->getAccessToken();
+	} catch(Facebook\Exceptions\FacebookResponseException $e) {
+		// When Graph returns an error
+		echo 'Graph returned an error: ' . $e->getMessage();
+		exit;
+	} catch(Facebook\Exceptions\FacebookSDKException $e) {
+		// When validation fails or other local issues
+		echo 'Facebook SDK returned an error: ' . $e->getMessage();
+		exit;
+	}
+
+	if(empty($accessToken)&&!empty($_SESSION['accessToken']))
+	{
+		$accessToken = $_SESSION['accessToken'];
+	}
+	else if(!empty($accessToken))
+	{
+		$_SESSION['accessToken'] = $accessToken;
+	}
+	else if(!empty($accessToken)&&!empty($_SESSION['accessToken']))
+	{
+		echo "accessToken error";
+		exit;
+	}
+	$fb->setDefaultAccessToken($accessToken);
+}
+?>
+	<script>
+		window.history.replaceState( {} , 'PaymentProcess', 'http://mommyssecret.tw/FBParserCallBack.php' );
+	</script>
+<?php
+	try {
+		$response = $fb->get('/me');
+		$userNode = $response->getGraphUser();
+	} catch(Facebook\Exceptions\FacebookResponseException $e) {
+		// When Graph returns an error
+		echo 'Graph returned an error: ' . $e->getMessage();
+		exit;
+	} catch(Facebook\Exceptions\FacebookSDKException $e) {
+		// When validation fails or other local issues
+		echo 'Facebook SDK returned an error: ' . $e->getMessage();
+		exit;
+	}
+	$fbAccount = $userNode->getName();
+	if(($fbAccount == 'Gill Fang')||
+	    ($fbAccount == 'JoLyn Dai')||
+	    ($fbAccount == '王雅琦')||
+	    ($fbAccount == 'Queenie Tsan')||
+	    ($fbAccount == '熊會買')||
+	    ($fbAccount == '熊哉')||
+	    ($fbAccount == '熊會算')||
+	    ($fbAccount == '古振平')||
+	    ($fbAccount == 'Keira Lin'))
+	{
+	    	 
+//         echo $userNode->getId();	
+	}
+	else
+	{
+	    echo "$fbAccount : 你不是管理者";
+	    exit;
+	}
+	//get feed
+
+	
+	try {
+	    $response = $fb->get("/607414496082801/feed?fields=id,created_time,message,comments&since=". date("Y-m-d", strtotime("-1 months")). "&offset=0");
+	} catch(Facebook\Exceptions\FacebookResponseException $e) {
+	    // When Graph returns an error
+	    echo 'Graph returned an error: ' . $e->getMessage();
+	    exit;
+	} catch(Facebook\Exceptions\FacebookSDKException $e) {
+	    // When validation fails or other local issues
+	    echo 'Facebook SDK returned an error: ' . $e->getMessage();
+	    exit;
+	}
+	$result = $response->getDecodedBody();
+ 
+	$pagesEdge = $response->getGraphEdge();
+	
+	do {
+	    foreach ($pagesEdge as $page) {
+	        echo substr($page['message'], 0, 60);
+	        echo "<br>";
+	        echo $page['created_time']->format('Y-m-d');
+	        echo "<br>";
+	        
+	        $comments = $page['comments'];
+	        do {
+	               for($i = 0; $i < count($comments); $i++)
+	               {
+	                   echo $comments[$i]["from"]["name"];
+	                   echo " ";
+	                   echo $comments[$i]["from"]["id"];
+	                   echo " ";
+	                   echo $comments[$i]["message"];
+	                   echo "<br>";
+	               }
+	        } while ($comments = $fb->next($comments));
+	    }
+	} while ($pagesEdge = $fb->next($pagesEdge));	
+	
+    
+		
+?>
